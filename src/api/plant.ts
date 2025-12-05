5461258fe051c857efed2ae758c94bb69aeb4cd1
Pannawich Thamart <MadManJJ4949@gmail.com>
2025-12-06 01:20:07 +0700

    fix: notification not run on watering plant

--- src/api/plant.ts
+++ src/api/plant.ts
@@ -1,16 +1,31 @@
 import type { SensorData } from "../types/SensorData";
 import { sendLineNotification } from "./notification";
-const ESP32_API_URL = import.meta.env.VITE_ESP32_API_URL || "http://172.20.10.2";
+
+const EXPRESS_API_URL = import.meta.env.VITE_EXPRESS_API_URL || "http://localhost:3000";
 
 const fetchWater = async (data: SensorData) => {
   console.log("Fetching water endpoint...");
   try {
     console.log("Sending request to /water...");
-    const response = await fetch(`${ESP32_API_URL}/water`);
+
+    const endpoint = `${EXPRESS_API_URL}/api/water`;
+    // Send a POST request to the /water endpoint with the sensor data
+    const response = await fetch(`${endpoint}`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data), // Send the sensor data in the request body
+    });
+
     if (!response.ok) {
       throw new Error(`HTTP error! status: ${response.status}`);
     }
+
+    // Send a notification with the sensor data
+    console.log("Sending notification with sensor data...");
     await sendLineNotification(data);
+    console.log("Watering request sent successfully.");
   } catch (error) {
     console.error("Error fetching water endpoint:", error);
   }
